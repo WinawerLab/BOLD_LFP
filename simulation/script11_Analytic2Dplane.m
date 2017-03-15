bea69bbdcb55766594def40d318db7fd6b05505d
dorahermes <dorahermes@gmail.com>
2017-03-14 22:08:56 -0700

    adjuste 2D figure script

--- simulation/script11_Analytic2Dplane.m
+++ simulation/script11_Analytic2Dplane.m
@@ -2,7 +2,7 @@ m = 2;      % mean
 v = 2;      % variance
 s = .1;     % coherence
 n = 5;      % number of neurons
-T = 10000;  % number of time points
+T = 1000;   % number of time points
 dt = 1/T;
 
 % covariance matrix
@@ -25,8 +25,72 @@ pw_pred  = (m^2 + v)*n;
 cpw_pred = (m^2 + v*s)*(n^2-n);
 disp([pw_pred cpw_pred])
 
+disp('note that these are only the same if 1 sec of data')
+
 %%
-%% PLOT NICE 2D function
+%% Plot total power (LFP), power (BOLD) and cross-power
+%%
+figure('Position',[0 0 700 250])
+
+fs = 10;    % font size
+n = 10;     % number of neurons
+[v, s] = meshgrid(linspace(0,1,10));
+
+for ii = 1:2
+    
+    if ii == 1, m = 0; else m = 1; end
+    
+    pw_pred  = (m^2 + v)*n;
+    cpw_pred = (m^2 + v.*s)*(n^2-n);
+    
+    subplot(1,3,1),set(gca, 'FontSize', fs);
+    total_pw = cpw_pred+pw_pred;
+    if ii==2
+        mesh(v, s, total_pw, total_pw - min(total_pw(:))),  hold on
+    else
+        surf(v, s, total_pw, total_pw - min(total_pw(:))),  hold on
+    end
+    title('Total Power (LFP)')
+    zlim([0 200])
+    
+    subplot(1,3,2), set(gca, 'FontSize', fs);
+    if ii==2
+        sH = mesh(v, s, pw_pred, pw_pred - min(pw_pred(:)));  hold on
+    else
+        sH = surf(v, s, pw_pred, pw_pred - min(pw_pred(:)));  hold on
+    end
+    set(sH, 'FaceLighting', 'gouraud')
+    title('Power (BOLD)')
+    zlim([0 200]) 
+    
+    subplot(1,3,3),set(gca, 'FontSize', fs);
+    if ii==2
+        mesh(v, s, cpw_pred, cpw_pred-min(cpw_pred(:))), hold on
+    else
+        surf(v, s, cpw_pred, cpw_pred-min(cpw_pred(:))), hold on
+    end
+    title('Cross Power')
+    zlim([0 200])
+end
+
+for k = 1:3
+    subplot(1,3,k)
+    h=xlabel('Variance (\sigma^2)'); set(h, 'rotation', 10)
+    h=ylabel('Correlation (\rho)');set(h, 'rotation', -30)
+    alpha(.8)
+    view(-30,10)
+    set(gca,'ZTick',[0:20:200])
+end
+
+colormap parula
+
+set(gcf,'PaperPositionMode','auto')
+print('-depsc','-r300',['../figures/analytic/2D_LFP_BOLD_CPW'])
+print('-dpng','-r300',['../figures/analytic/2D_LFP_BOLD_CPW'])
+
+
+%%
+%% Plot nice 2D function of power (BOLD), cross-power and total power (LFP)
 %%
 
 fH = figure; clf; pos=get(fH, 'Position');
